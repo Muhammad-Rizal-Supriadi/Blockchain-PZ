b7cc9c79583e23b0c325b33c6e365bd8fa343cc3
rizal.productzilla <mrsupriadi2000@gmail.com>
2023-02-02 16:47:09 +0700

    truffle

---
+++ contracts/MyData.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.7.0;
+
+import "./bigchaindb-connection.js";
+
+contract MyData {
+    function storeData(string memory data) public {
+        // Create a new asset
+        const asset = { data: { message: data } };
+
+        // Define the transaction
+        const tx = BigchainDB.Transaction.makeCreateTransaction(
+            asset,
+            {},
+            [ BigchainDB.Transaction.makeOutput(
+                BigchainDB.Transaction.makeEd25519Condition(publicKey))
+            ],
+            publicKey
+        );
+
+        // Sign the transaction
+        const signedTransaction = BigchainDB.Transaction.signTransaction(tx, privateKey);
+
+        // Send the transaction to the BigchainDB network
+        conn.postTransactionCommit(signedTransaction);
+    }
+}
